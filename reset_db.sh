536a07a82b364e145a44514275c99fb5c7acb1a7
Jeremy Krovitz <jkrovitz@macalester.edu>
2018-11-03 16:49:07 -0500

    Fixed reset_db.sh file.

--- reset_db.sh
+++ reset_db.sh
@@ -1,7 +1,6 @@
 find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
 find . -path "*/migrations/*.pyc"  -delete
-# rm db.sqlite3
-$ [ -f db.sqlite3 ] && rm db.sqlite3 || echo "The file db.sqlite3 does not exist in your directory or has already been removed."
+rm db.sqlite3
 python manage.py makemigrations
 python manage.py migrate
 echo "The database has been reset. Please remember to create a new superuser by typing in the command: \"python manage.py createsuperuser\" before running the server."
