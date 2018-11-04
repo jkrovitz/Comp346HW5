ac1ac3cc084e8e9b2ef06aeaf42086e18f8caae0
Jeremy Krovitz <jkrovitz@macalester.edu>
2018-11-04 17:12:59 -0600

    Added a virtual environment

---
+++ myVenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/jkrovitz/DocumentsJeremyMacBookPro/git/homework5/Comp346HW5/myVenv/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
